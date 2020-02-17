5fb1b7e58adef20118922763daa21003617a9e88
Mustafa Siddiqui <mustafa.ar.siddiqui@gmail.com>
2020-02-17 10:54:25 +0530

    Forgot to commit :P

---
+++ char.py
@@ -0,0 +1,112 @@
+import pygame
+
+class python_game_characters():
+    def __init__(self, x=360, y=512):
+        self.screen_width = 750
+        self.screen_height = 750
+        self.boat=[[True, 0],[True, 187.5],[True, 375],[True, 562.5]]
+        self.width=58
+        self.height=80
+        self.x=x
+        self.y=y
+        self.velocity=2
+        self.run=True
+        self.isWalk=False
+        self.left=False
+        self.character_movements_animation()
+        self.game_setup()
+
+    def move_boat(self):
+        boat_velocity = 2.5
+        river_count=0
+        for i in self.boat:
+            if i[0]==True and i[1]+boat_velocity>self.screen_width:
+                i[0]=False
+            if i[0]==False and i[1]<boat_velocity:
+                i[0]=True
+            if i[0] == True:
+                i[1]+=boat_velocity
+            else:
+                i[1]-=boat_velocity
+            boat_velocity-=0.5
+            pygame.draw.rect(self.win, (0,255,0), [(i[1]),(2*river_count+1)*64,64,64])
+            river_count+=1
+
+
+    def render_obstacles(self):
+        bridge_counter=0
+        river_counter=0
+        for i in range(9):
+            if i % 2 == 0:
+                for j in range(5):
+                    if bridge_counter%2 == 0:
+                        pygame.draw.rect(self.win, (255,0,0), [(j*150)+16,(i*64)+16,32,32])
+                    else:
+                        pygame.draw.rect(self.win, (255,0,0), [(j*150)+80,(i*64)+16,32,32])
+                bridge_counter+=1
+            else:
+                self.move_boat()
+    def surroundings(self):
+        self.river_height=self.bridge_height=64
+        self.bridge_width=64
+        self.river_width=250
+        for j in range(((self.screen_height*8)//10)//(self.river_height)):
+            if j % 2 == 1:
+                for i in range(self.screen_width//self.river_width+1):
+                    self.win.blit((pygame.image.load('./sprites/Ocean_SpriteSheet.png')),(self.river_width*i,(self.river_height*j)))
+            else:
+                for i in range((self.screen_width//self.bridge_width)+1):
+                    self.win.blit((pygame.transform.rotate(pygame.image.load('./sprites/bridge.png'),90)),(self.bridge_width*i,(self.bridge_height*j)))
+
+    def game_setup(self):
+        pygame.init()
+        self.win=pygame.display.set_mode((self.screen_width,self.screen_height))
+        pygame.display.set_caption("River cross 1 v 1")
+        while self.run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.run=False
+                    pygame.quit()
+            self.user_movement_input()
+    
+    def character_drawing(self):
+        self.win.fill((0,0,0))
+        self.surroundings()
+        self.render_obstacles()
+        if self.walkcount+1 > 30:  self.walkcount=0
+        if self.idle_count+1 > 20: self.idle_count=0
+        if self.isWalk == True and self.left==False:    self.win.blit(self.run_right[self.walkcount//5], (self.x,self.y))
+        elif self.left==True:    self.win.blit(self.run_left[self.walkcount//5], (self.x,self.y))
+        else:   self.win.blit(self.idle[self.idle_count//5], (self.x, self.y))
+        pygame.display.update()
+    
+    def user_movement_input(self):
+        keys=pygame.key.get_pressed()
+        self.walkcout=0
+        self.idle_count=0
+        if keys[pygame.K_LEFT] and self.x > self.velocity:                                                 self.x-=self.velocity; self.walkcount+=1; self.isWalk=True; self.idle_count=0; self.left=True
+        elif keys[pygame.K_RIGHT] and self.x < self.screen_width-self.width-self.velocity:                 self.x+=self.velocity; self.walkcount+=1; self.isWalk=True; self.idle_count=0; self.left=False
+        elif keys[pygame.K_UP] and self.y > self.velocity:                                                 self.y-=self.velocity; self.walkcount=0;  self.isWalk=False; self.idle_count+=1
+        elif keys[pygame.K_DOWN] and self.y < self.screen_height*0.8-1.25*self.height:                                      self.y+=self.velocity; self.walkcount=0;  self.isWalk=False; self.idle_count+=1
+        else:                                                                                              self.walkcount=0; self.isWalk=False; self.idle_count+=1
+        self.character_drawing()
+
+    def character_movements_animation(self):
+        self.idle=[pygame.image.load('./sprites/idle_0.png'),pygame.image.load('./sprites/idle_1.png'),pygame.image.load('./sprites/idle_2.png'),pygame.image.load('./sprites/idle_3.png')]
+        self.run_right=[pygame.image.load('./sprites/run_0.png'), pygame.image.load('./sprites/run_1.png'), pygame.image.load('./sprites/run_2.png'),pygame.image.load('./sprites/run_3.png'),pygame.image.load('./sprites/run_4.png'),pygame.image.load('./sprites/run_5.png')]
+        self.run_left=[]
+        for i in range(6):
+            self.run_left.append(pygame.transform.flip(self.run_right[i], True, False))
+# ---------------------------------------------Python Game environment------------------------------------------------
+# class python_game_environment():
+#     def __init__(self):
+#         self.screen_width = 750
+#         self.screen_height = 750
+        # self.river_image=
+# ---------------------------------------------Driver Function------------------------------------------------
+
+def main():
+    ob=python_game_characters()
+
+if __name__ == '__main__':
+    main()
